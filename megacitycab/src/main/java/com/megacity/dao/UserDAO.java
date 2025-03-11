e8e2a0764b91b0cc4042a42b8ebccdfcbccca7e3
Rushdha <rushdhaakbar82@gmail.com>
2025-03-11 23:13:19 +0530

    green code for userdao for user login

--- megacitycab/src/main/java/com/megacity/dao/UserDAO.java
+++ megacitycab/src/main/java/com/megacity/dao/UserDAO.java
@@ -1,18 +1,35 @@
 package com.megacity.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.megacity.model.User;
 
 public class UserDAO {
 
-	public UserDAO(Connection mockConnection) {
-		// TODO Auto-generated constructor stub
-	}
+    private Connection connection;
 
-	public User getUserByEmail(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public UserDAO(Connection connection) {
+        this.connection = connection;
+    }
 
+    public User getUserByEmail(String email) {
+        String query = "SELECT * FROM user WHERE email = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, email);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                User user = new User();
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password")); 
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null; 
+    }
 }
