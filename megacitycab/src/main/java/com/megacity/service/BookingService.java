22ab34afbc1da289b5253cb9145ac0192969a7ec
Rushdha <rushdhaakbar82@gmail.com>
2025-03-13 22:34:24 +0530

    Green : Test pass for BokkingServiceTest, implementing createbooking() in createbooking in bookingService class

--- megacitycab/src/main/java/com/megacity/service/BookingService.java
+++ megacitycab/src/main/java/com/megacity/service/BookingService.java
@@ -1,14 +1,14 @@
 package com.megacity.service;
 
 import com.megacity.model.Booking;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BookingService {
+    private List<Booking> bookings = new ArrayList<>();
 
-	public Booking createBooking(Booking booking) {
-        return null; 
+    public Booking createBooking(Booking booking) {
+        bookings.add(booking);
+        return booking; 
     }
-		
-
-	
-
 }
